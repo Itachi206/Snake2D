3959f248b14e7acf515556d35e9342cd07f93f43
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-02-07 12:08:25 +0530

    snake, food added screen wrapping is added

---
+++ Snake2D/Assets/Scripts/FoodController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FoodController : MonoBehaviour
+{
+    public BoxCollider2D gridArea;
+
+    private void Start()
+    {
+        StartCoroutine(RandomizeFoodPosition());
+    }
+
+    IEnumerator RandomizeFoodPosition()
+    {
+        //box collider property Bound to get the size of the area
+        Bounds boundGridArea = this.gridArea.bounds;
+
+        //random number genrator for food using unity inbuilt function Random()
+        float x = Random.Range(boundGridArea.min.x, boundGridArea.max.x);
+        float y = Random.Range(boundGridArea.min.y, boundGridArea.max.y);
+
+        //assigm food position to this coordinates and round those values to get whole numbers
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+        yield return new WaitForSeconds(.1f);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //when snake collide with the food it should again regenerate at new position
+        if(other.tag == "Player")
+        {
+            StartCoroutine(RandomizeFoodPosition());
+        }
+    }
+}
